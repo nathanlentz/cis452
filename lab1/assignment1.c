ddeb1a518c7b66af88d69978fd386dac723c08a2
Nate Lentz <nathanlentz@outlook.com>
2017-01-14 11:46:05 -0500

    Adding lab 1 and beginning assignment 1

---
+++ lab1/assignment1.c
@@ -0,0 +1,22 @@
+/*********************************
+* CIS 452 W17: Lab 1 - Assignment
+* @author: Nathan Lentz
+* 01.12.17
+**********************************/
+
+/* TODO REMOVE COMMENT BLOCK
+
+Research 
+http://man7.org/linux/man-pages/man2/syscalls.2.html
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main()
+{
+    
+}
