6cf7ed8f43c3016a838a7a61b1d2e38f6bc5462e
Nate Lentz <lentzna@mail.gvsu.edu>
2017-01-19 16:51:09 -0500

    Working on lab 2

---
+++ lab2/sample1.c
@@ -0,0 +1,14 @@
+/* Sample Program 1 */ 
+
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+
+int main() 
+{ 
+    puts("Before fork"); 
+    fork(); 
+    sleep(10);
+    puts("After fork"); 
+    return 0; 
+}
