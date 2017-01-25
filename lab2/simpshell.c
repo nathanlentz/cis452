77cd6a6d0fb802f847eac302dfdf3d6b1630616a
Nate Lentz <lentzna@mail.gvsu.edu>
2017-01-25 12:32:13 -0500

    Commenting blueprints of how I think simple shell will be done

--- lab2/simpshell.c
+++ lab2/simpshell.c
@@ -25,7 +25,7 @@ char** read_command();
 
 int main(int argc, char* argv[]) 
 {  
-	pid_t pid;
+	pid_t pid, child
 	int status;
 	char** command;
 
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
 	while(1){
 
 		// Call read command and parse user input 
-		//command = read_command();
+		command = read_command();
 
 		// Exit if user types quit
 	    if(!strcmp(command[0],"quit") && command != NULL){
@@ -46,16 +46,25 @@ int main(int argc, char* argv[])
     		perror("Fork Failed");
     		exit(1);
     	}
+    	// The Child
     	else if(pid == 0){ 
-    		// We are the child, do stuff
+    		// We are the child, do stuff 
+            // TODO: Implement execvp()
+
 
     	}
+    	// The Parent. Wait for child to execute and get data for usage
+    	// waitpid -> https://linux.die.net/man/2/waitpid
     	else if(pid > 0){
     		// We are the parent, do stuff
-    	}
     		
+    		// Wait for 
+    		waitpid(-1, &status, 0);
 
-    	
+    		// TODO: Get data for usage
+            
+
+    	}
 	}
 	// Return memory used for command as program exits
 	free(command);
@@ -71,5 +80,42 @@ void display_console()
 // Parse command in arguments sent to child process
 char** read_command()
 {
+	char buf[LINESIZE];	
+    char* word;
 
+    // An array of pointers 
+	char** command;
+
+    while(1){
+        display_console();
+
+        // Get user input
+        fgets(buf, LINESIZE, stdin)
+
+        // Was a command entered?
+        if(strlen(buf)==1){
+            continue;
+        }
+
+        // TODO: Get rid of '\0' at end 
+
+        // TODO: Check if command was longer than line length - 2
+
+        break;
+    }
+
+    // We now have a command. Allocate memory
+    command = malloc(sizeof(char*));
+    word = strtok(buf, " ");
+
+    // Iterate thorugh arguments, reallocate space for pointer array
+    // allocate space for pointer to string in the pointer array, copy
+    // the string to the pointer array
+    int i = 0;
+    while(){
+        // Do stuff above
+        i++
+    }
+
+    free(word);
 }	
